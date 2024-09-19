3e6d2104e9d1c9bc07bf457ce42be760f5fdaf3d
AbdulatipA <ataev0461@gmail.com>
2024-09-20 00:07:57 +0300

    lessonObject_commit1

--- src/main/java/org/example/classObject/Main.java
+++ src/main/java/org/example/classObject/Main.java
@@ -1,8 +1,7 @@
 package org.example.classObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
