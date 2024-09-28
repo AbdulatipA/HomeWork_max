6222c134ab931ea3bd6b5e624a10c5b7848a5ab6
AbdulatipA <ataev0461@gmail.com>
2024-09-28 22:23:31 +0300

    lesson_MethodsClasses_commit_2

--- src/main/java/org/example/typeofClasses/localClass/findMax/ArrayProcessor.java
+++ src/main/java/org/example/typeofClasses/localClass/findMax/ArrayProcessor.java
@@ -5,20 +5,20 @@ public class ArrayProcessor {
     public void findMax(int[] arr){
 
          class MaxFinder{
+             int maxCount;
+
+             int getMaxCount(){
+                for (int i : arr) {
+                    if (i > maxCount) {
+                        maxCount = i;
+                    }
+                }
+                return maxCount;
+            }
 
         }
+        MaxFinder maxFinder = new MaxFinder();
+        int result = maxFinder.getMaxCount();
+        System.out.println(result);
     }
 }
-
-
-
-
-
-
-//    private int max;
-//        for (int i : arr) {
-//            if(i > max) {
-//                max = i;
-//            }
-//        }
-//        System.out.println(max);
--- src/main/java/org/example/typeofClasses/localClass/findMax/Main.java
+++ src/main/java/org/example/typeofClasses/localClass/findMax/Main.java
@@ -1,13 +1,8 @@
 package org.example.typeofClasses.localClass.findMax;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         ArrayProcessor arrayProcessor = new ArrayProcessor();
-//        arrayProcessor.findMax(new int[]{1,2,3,4,5,6,7,8,9,10});
-        int[] arr = {10, 2, 1};
-        arrayProcessor.findMax(arr);
-        System.out.println(Arrays.toString(arr));
+        arrayProcessor.findMax(new int[]{4, 6, 8, -1, 10, 0, 20});
     }
 }
